158e03527bcf02d579fab9741c31f172d2b662f4
Nathan Denny <nathandenny@uvic.ca>
2020-07-11 14:56:17 -0700

    Added additional functions for finding P,Q, D, E

--- src/unoptimized_rsa.c
+++ src/unoptimized_rsa.c
@@ -2,6 +2,68 @@
 #include <math.h>
 #include "rsa.h"
 
+//First two prime numbers (We are using bitwidth 128 bits)
+int P;
+int Q;
+
+//Public and private exponents (We are using bitwidth 128 bits)
+int D;
+int E;
+
+bool isPrime(int a){
+    for(int i = 2; i < a/2; i++){
+		if( a%i == 0 && a != 0) return 0;
+	}
+	return 1;
+}
+
+//Generates P and Q for RSA
+int generatePrimes(){
+    	do{
+            P = rand() % 32767;	
+        }while(!isprime(P));
+	    do{	
+            Q = rand() % 32767;	
+        }while(!isprime(Q));
+	return;
+}
+
+//used for checking relative primes
+int greatestCommonDenom(int a, int b){
+    int i;
+    int greatest;
+	for(i = 1; i <= a && i <= b; ++i) {
+		if(a % i == 0 && b % i == 0) {greatest = i;}
+	}
+	return gcd;
+}
+
+//Used for checking that E and (P-1)(Q-1) are not relatively prime
+bool isRelativelyPrime(int a, int b){
+    if (gcd(a,b) == 1) return 1;
+	return 0;
+}
+
+//Generate E
+void generateE(){
+    E = 0;
+    while(1){
+        if(isRelativelyPrime(E, (P - 1)*(Q - 1))){ return;}
+        else{
+            //E = next prime number
+        }
+    }
+}
+
+//Generate D
+int generateD(){
+    // int X = 0;
+    // int temp = 0;
+    // while(1){
+    //     temp = (X * (P - 1)(Q - 1) + 1)/E
+    // }
+}
+
 //Montgomery Modular Multiplication
 int mmm(int X, int Y, int M, int bitLength){
     int T = 0;
@@ -11,9 +73,7 @@ int mmm(int X, int Y, int M, int bitLength){
         n = T ^ (Xi & Y);
         T = (T + (Xi*Y) + (n*M)) >> 1;
     }
-    if(T >= M ){
-        T = T - M;
-    }
+    if(T >= M ){T = T - M};
     return T;
 }
 
@@ -22,6 +82,14 @@ int me(){
 
 }
 
+//Functions for easy encryption/decryption of a message
+int encrypt(int message){
+    return  me();
+}
+
+int decrypt(int encoded_message){
+    return me();
+}
 
 int main(){
 
