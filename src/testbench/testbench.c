5c9fed380d9993e1e5424cd1e2ad8e5a61b5cb0b
Elliot <elliot.mcsmythurs@plurilock.com>
2020-07-22 14:16:12 -0700

    Removes deprecated tests

--- src/testbench/testbench.c
+++ src/testbench/testbench.c
@@ -4,41 +4,6 @@
 
 int tests_run = 0;
 
-static char * test_isPrime_p() {
-	int prime = 1543763561;
-	mu_assert("error, isPrime returned false", isPrime(prime));
-	return 0;
-}
-
-static char * test_isPrime_n() {
-	int composite = 1543763562;
-	mu_assert("error, isPrime returned true", isPrime(composite) == false);
-	return 0;
-}
-
-static char * test_greatestCommonDenom() {
-	int a = 644839;
-	int b = 9763;
-	int expected_gcd = 13;
-	int gcd = greatestCommonDenom(a, b);
-	mu_assert("error, gcd != 13", gcd == expected_gcd);
-	return 0;
-}
-
-static char * test_isRelativelyPrime_p() {
-	int a = 655839;
-	int b = 9764;
-	mu_assert("error, isRelativelyPrime returned false", isRelativelyPrime(a, b));
-	return 0;
-}
-
-static char * test_isRelativelyPrime_n() {
-	int a = 655838;
-	int b = 9764;
-	mu_assert("error, isRelativelyPrime returned true", isRelativelyPrime(a, b) == false);
-	return 0;
-}
-
 static char * test_mmm() {
 	int a = 4;
 	int b = 7;
@@ -67,11 +32,6 @@ static char * test_encrypt_decrypt() {
 }
 
 static char * all_tests() {
-	mu_run_test(test_isPrime_p);
-	mu_run_test(test_isPrime_n);
-	mu_run_test(test_greatestCommonDenom);
-	mu_run_test(test_isRelativelyPrime_p);
-	mu_run_test(test_isRelativelyPrime_n);
 	mu_run_test(test_mmm);
 	mu_run_test(test_me);
 	mu_run_test(test_encrypt_decrypt);
