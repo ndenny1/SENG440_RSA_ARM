ed9bbdb2e1a85b72fce0e29c9e7a4c984887c7da
Nathan Denny <nathandenny@uvic.ca>
2020-08-08 12:34:48 -0700

    Updated to 256 bit

--- src/unoptimized_rsa.c
+++ src/unoptimized_rsa.c
@@ -1,90 +1,76 @@
 #include "unoptimized_rsa.h"
 
 //modulus
-uint128_t * M;
+uint256_t * M;
 //private
-uint128_t * D;
+uint256_t * D;
 //public
-uint128_t * E;
+uint256_t * E;
 
 
 //used to count bitlength for Montgomery Modular Multiplication
-uint8_t count_num_bits(uint128_t* value){
-
-    uint8_t count = 0;
-    uint32_t h = value->high;
-    uint32_t m1 = value->mid1;
-    uint32_t m2 = value->mid2;
-    uint32_t l = value->low;
-
-    while(h > 0) {
-        count ++;
-        h >>= 1;
-    }
-    while(m1 > 0) {
-        count ++;
-        m1 >>= 1;
-    }
-    while(m2 > 0) {
-        count ++;
-        m2 >>= 1;
-    }
-    while(l > 0) {
-        count ++;
-        l >>= 1;
-    }
-    printf("Num bits: %d\n", count);
+uint16_t count_num_bits(uint256_t* value){
+    uint16_t count = 0;
+    uint16_t block = 0;
+    for (; block < 8; block++) {
+		uint32_t curBlock = value->blocks[block];
+        while(curBlock){
+            count++;
+            curBlock >>= 1;
+        }
+	}
+    // printf("Num bits: %d\n", count);
     return count;
 }
 
 //Montgomery Modular Multiplication
-uint128_t* mmm(uint128_t* X, uint128_t* Y, uint128_t* M, uint32_t bitLength){
-    uint128_t * T = cast_to_uint128(0);
-    uint128_t * n = cast_to_uint128(0);
+uint256_t* mmm(uint256_t* X, uint256_t* Y, uint256_t* M, uint32_t bitLength){
+    uint256_t * T = cast_to_uint256(0);
+    uint256_t * n = cast_to_uint256(0);
     //loop fusion not possible, nor loop fission
-    for(uint32_t a=0; a < bitLength; a++){
-        uint128_t * Xi = and_uint128(rshift_uint128(X, a), cast_to_uint128(1));
-        n = xor_uint128(and_uint128(T, cast_to_uint128(1)), and_uint128(Xi, and_uint128(Y, cast_to_uint128(1))));
-        T = add_uint128(T, add_uint128(mul_uint128(Xi, Y), mul_uint128(n, M)));
+    for(uint16_t a=0; a < bitLength; a++){
+        uint256_t * Xi = and_uint256(rshift_uint256(X, a), cast_to_uint256(1));
+        n = xor_uint256(and_uint256(T, cast_to_uint256(1)), and_uint256(Xi, and_uint256(Y, cast_to_uint256(1))));
+        T = add_uint256(T, add_uint256(mul_uint256(Xi, Y), mul_uint256(n, M)));
     }
-    if(gte_uint128(T, M)){T = sub_uint128(T, M);}
+    if(gte_uint256(T, M)){T = sub_uint256(T, M);}
     return T;
 
 }
 
 //Modular Exponentiation
-uint128_t* me(uint128_t* message, uint128_t* key, uint128_t* modulus){
-	if (uint128_equal_to_zero(modulus)){
+uint256_t* me(uint256_t* message, uint256_t* key, uint256_t* modulus){
+	if (uint256_equal_to_zero(modulus)){
 		return 0;
 	}
-    uint8_t key_bits = count_num_bits(key);
-    uint8_t mod_bits = count_num_bits(modulus);
-    uint128_t* r_squared = cast_to_uint128(1);
-    for(uint8_t a = 0; a < mod_bits*2; a++){
-        //need modulus here
-        r_squared = mod_uint128(mul_uint128(r_squared,cast_to_uint128(2)), modulus);
+    uint16_t key_bits = count_num_bits(key);
+    uint16_t mod_bits = count_num_bits(modulus);
+    uint256_t* r_squared = cast_to_uint256(1);
+    // uint256_t* r_squared = mod_uint256(cast_to_uint256((1 << (2*mod_bits))), modulus);
+    for(uint16_t a = 0; a < mod_bits*2; a++){
+        r_squared = mod_uint256(mul_uint256(r_squared,cast_to_uint256(2)), modulus);
     }
-	uint128_t* C = mmm(cast_to_uint128(1), r_squared, modulus, mod_bits);
-    uint128_t* S = mmm(message, r_squared, modulus, mod_bits);
-    for (uint8_t i = 0; i < key_bits; i++) {
-        uint128_t* key_i = and_uint128(rshift_uint128(key, i),cast_to_uint128(1));
-        if (equal_uint128(key_i, cast_to_uint128(1))) {
+	uint256_t* C = mmm(cast_to_uint256(1), r_squared, modulus, mod_bits);
+    uint256_t* S = mmm(message, r_squared, modulus, mod_bits);
+    for (uint16_t i = 0; i < mod_bits; i++) {
+        uint256_t* key_i = and_uint256(rshift_uint256(key, i),cast_to_uint256(1));
+        if (equal_uint256(key_i, cast_to_uint256(1))) {
             C = mmm(C, S, modulus, mod_bits);
         }
         S = mmm(S, S, modulus, mod_bits);
     }
-    C = mmm(C,cast_to_uint128(1), modulus, mod_bits);
+    C = mmm(cast_to_uint256(1), C, modulus, mod_bits);
 	return C;
 }
 
 //Functions for easy encryption/decryption of a message
-uint128_t* encrypt(uint128_t* message){
-    M = uint128_init(0xd4549557, 0x1f28252d, 0x003e390a, 0x0f7d1783);
-    E = uint128_init(0x00000000, 0x00000000, 0x00000000, 0x00010001);
-    //we can probably get rid of this declaration, I was just using it to better understand the algorithm
-    uint128_t* T = message;
+uint256_t* encrypt(uint256_t* message){
+    uint32_t mArr[8] = {0x00000000,0x00000000,0x00000000,0x00000000,0xd4549557, 0x1f28252d, 0x003e390a, 0x0f7d1783};
+    uint32_t eArr[8] = {0x00000000,0x00000000,0x00000000,0x00000000,0x00000000, 0x00000000, 0x00000000, 0x00010001};
+    M = uint256_init(mArr);
+    E = uint256_init(eArr);
     //algorithm is  C = T^E mod M (C is cypertext, T is plaintext, E is public key, M is modulus)
-    return  me(T, E, M);
+    return  me(message, E, M);
 }
 //Modulus:
 //d4549557 1f28252d 003e390a 0f7d1783
@@ -94,23 +80,23 @@ uint128_t* encrypt(uint128_t* message){
 
 //Public
 //00000000 00000000 00000000 00010001
-uint128_t* decrypt(uint128_t* encoded_message){
-    M = uint128_init(0xd4549557, 0x1f28252d, 0x003e390a, 0x0f7d1783);
-    D = uint128_init(0x75312a2c,0x27da3489,0x9c87f263, 0xd9701aa1);
-    //we can probably get rid of this declaration, I was just using it to better understand the algorithm
-    uint128_t* C = encoded_message;
+uint256_t* decrypt(uint256_t* encoded_message){
+    uint32_t mArr[8] = {0x00000000,0x00000000,0x00000000,0x00000000,0xd4549557, 0x1f28252d, 0x003e390a, 0x0f7d1783};
+    uint32_t dArr[8] = {0x00000000,0x00000000,0x00000000,0x00000000,0x75312a2c,0x27da3489,0x9c87f263, 0xd9701aa1};
+    M = uint256_init(mArr);
+    D = uint256_init(dArr);
     //algorithm is  T = C^D mod M (C is cypertext, T is plaintext, D is private key, M is modulus)
-    return me(C, D, M);
+    return me(encoded_message, D, M);
 }
 
 
 int main() {
-    // hello world
-    uint128_t* message = cast_to_uint128(1289372189);
-    print_uint128("Initial Message: ", message);
-    uint128_t* encoded = encrypt(message);
-    print_uint128("Encoded Message: ", encoded);
-    uint128_t* decoded = decrypt(encoded);
-    print_uint128("Decoded Message (should be same as initial): ", decoded);
+    uint32_t mes = (int)"Hello World";
+    uint256_t* message = cast_to_uint256(mes);
+    print_uint256("Initial Message: ", message);
+    uint256_t* encoded = encrypt(message);
+    print_uint256("Encoded Message: ", encoded);
+    uint256_t* decoded = decrypt(encoded);
+    print_uint256("Decoded Message: ", decoded);
     return 0;
 }   
--- src/unoptimized_rsa.h
+++ src/unoptimized_rsa.h
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdint.h>
-#include "int128.h"
+#include "int256.h"
 
 #ifndef bool
 	#include <stdbool.h>
 	#define bool	_Bool
 #endif
 
-uint8_t count_num_bits(uint128_t* value);
+uint16_t count_num_bits(uint256_t* value);
 
-uint128_t* mmm(uint128_t* X, uint128_t* Y, uint128_t* M, uint32_t bitLength);
+uint256_t* mmm(uint256_t* X, uint256_t* Y, uint256_t* M, uint32_t bitLength);
 
-uint128_t* me(uint128_t* message, uint128_t* key, uint128_t* modulus);
+uint256_t* me(uint256_t* message, uint256_t* key, uint256_t* modulus);
 
-uint128_t* encrypt(uint128_t* message);
+uint256_t* encrypt(uint256_t* message);
 
-uint128_t* decrypt(uint128_t* encoded_message);
+uint256_t* decrypt(uint256_t* encoded_message);
